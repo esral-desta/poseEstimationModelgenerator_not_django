7cc84598360ca0ea74612d42aac42f7fe2b8a2f6
esudesta <esudesta5@gmail.com>
2023-03-13 10:12:12 +0300

    added basic mediapipe pose estimation and tracking

--- app.py
+++ app.py
@@ -5,10 +5,26 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret,frame = cap.read()
-    cv2.imshow("MediaPipe Feed",frame)
-    if cv2.waitKey(10) & 0xff == ord("q"):
-        break
-cap.release()
-cv2.destroyAllWindows()
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret,frame = cap.read()
+        
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, 
+                                         mp_pose.POSE_CONNECTIONS,
+                                         mp_drawing.DrawingSpec(color=(245,117,66),thickness=2,circle_radius=2),
+                                         mp_drawing.DrawingSpec(color=(245,66,236),thickness=2,circle_radius=2)
+            )
+
+        cv2.imshow("MediaPipe Feed",image)
+        if cv2.waitKey(10) & 0xff == ord("q"):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
